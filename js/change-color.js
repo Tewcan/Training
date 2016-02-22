ba0cf53f5d61bb9070c036b70dfa18a65b6b1838
Eric Tew <Gold_Kirby@hotmail.com>
2016-02-21 21:15:40 -0800

    New Header Button
    I made a change to the header?

--- js/change-color.js
+++ js/change-color.js
@@ -1,1 +1,7 @@
 document.getElementById('whatever').style.color = 'blue'
+function fun() {
+	document.getElementById('whatever').style.color = 'red'
+}
+function header() {
+	document.getElementById('headingid').innerHTML = 'THE NEW HEADER!';
+}
